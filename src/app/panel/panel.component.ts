c20576eec8f260c61263a2f5bfbe48ef8e7cca03
Hadi <hadi.deris.h82@gmail.com>
2024-07-29 17:08:41 +0330

    create panel and layout

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
-  standalone: true,
-  imports: [],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.scss'
 })
--- src/app/panel/panel.module.ts
+++ src/app/panel/panel.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PanelRoutingModule } from './panel-routing.module';
+import { LayoutComponent } from './layout/layout.component';
+import { PanelComponent } from './panel.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PanelComponent,LayoutComponent,HeaderComponent,SidebarComponent,ContentComponent,FooterComponent],
   imports: [
     CommonModule,
-    PanelRoutingModule
+    PanelRoutingModule,
+    MaterialModule
   ]
 })
 export class PanelModule { }
