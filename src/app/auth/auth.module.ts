84f17ec648957d5e4d9d5fb339ee0032331528a0
Hadi <hadi.deris.h82@gmail.com>
2024-07-29 17:01:20 +0330

    create login form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [AuthComponent,LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+
   ]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import _ from "lodash";
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  form:FormGroup;
+  constructor(private router: Router,private fb: FormBuilder){
+    this.form = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+     })
+  }
+
+validForm = {
+  userName: 'hadi',
+  password: '123456',
+};
+  ngOnInit(): void {
+
+  }
+
+
+  submit(){
+   
+    (_.isEqual(this.validForm,this.form.value)) ? this.router.navigate(['/panel']) : '';
+
+  }
 
 }
